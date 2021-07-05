abab2f567273ef4c5aa3d25f8a8f5bf8b918f441
baotaian <1124258922@qq.com>
2021-07-05 09:26:13 +0800

    首次提交

---
+++ app/src/main/java/com/taian/autonet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.taian.autonet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
